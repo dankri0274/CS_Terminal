4c836c196e32c28bda54d9f001e759e3ee1fb112
dannyboy9934 <50834782+dannyboy9934@users.noreply.github.com>
2021-12-16 17:22:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace myProject {
+	class Program {
+		static void Main(string[] args) {
+			Console.Write("> ");
+			string? cmd = Console.ReadLine();
+
+			if (cmd == "set passwd") {
+				// Get the output from function getPass() and store it in a variable
+				//! Error: Cannot implicitly convert type 'void' to 'string'
+				string pass = getPass();
+				if (pass != null) {
+					Console.WriteLine(pass);
+				}
+				Console.WriteLine(pass);
+			}			
+		}
+
+		//* Hide input
+		static string getPass() {
+			string? password = null;
+			while (true) {
+				var key = Console.ReadKey(true);
+				if (key.Key == ConsoleKey.Enter)
+					break;
+				password += key.KeyChar;
+			}
+			return;
+		}
+	}
+}
